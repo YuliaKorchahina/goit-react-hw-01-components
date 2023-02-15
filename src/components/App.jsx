fa2c0888654ae2210108444a68a354bab575829c
YuliaKorchahina <ruda_nokia@ukr.net>
2023-02-15 16:09:22 +0200

    user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import Profile from './Profile/Profile';
+import user from '';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React 
+    <div>
+      <Profile User={user} />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+//import {Component} from 'react'
+//import User from '../../src/user.json';
+
+
+
+export const Profile = ({ user }) => {
+  return (
+    <div className="">
+      {/* {User.map(
+        (username, tag, location, avatar, stats, followers, views, likes) => (
+          <Profile
+            username={username}
+            tag={tag}
+            location={location}
+            avatar={avatar}
+            stats={stats}
+          />
+        )
+      )} */}
+    </div>
+  );
+};
